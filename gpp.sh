d50d28fcaaeb3f4159afcab07f945b1e0dfcbf64
shumaikunkun <s1711520@gmail.com>
2019-10-15 13:16:09 +0900

    first commit

---
+++ gpp.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "$1.cppを実行します\n"
+g++ $1.cpp
+./a.out
+echo "\n"
---
+++ test.cpp
@@ -0,0 +1,10 @@
+/* sample 1 */
+#include <iostream>
+using namespace std;
+int main()
+{
+string name;
+cout << "お名前は？\n";
+cin >> name;
+cout << "こんにちは、" << name << "さん！\n";
+}
