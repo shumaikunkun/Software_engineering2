361d479d54d029cf889143c8bf5c33b5c5dbbe74
shumaikunkun <s1711520@gmail.com>
2019-12-21 04:39:55 +0900

    fix k2

--- k2.cpp
+++ k2.cpp
@@ -14,7 +14,7 @@ class addressbook{
 public:
   addressbook();
   void listFriends();
-  void addFriend(string n);
+  void addFriend(string n, string a);
 };
 
 // constructor
@@ -22,14 +22,15 @@ addressbook::addressbook(){
   numberOfFriends = 0;
 }
 
-void addressbook::addFriend(string n){
+void addressbook::addFriend(string n, string a){  //input
   friends[numberOfFriends].name = n;
+  friends[numberOfFriends].address = a;
   numberOfFriends++;
 }
 
-void addressbook::listFriends(){
+void addressbook::listFriends(){  //output list
   for(int i = 0; i < numberOfFriends; i++){
-    cout << friends[i].name << "\n";
+    cout << friends[i].name << ":" << friends[i].address << "\n";
   }
 }
 
@@ -37,13 +38,15 @@ int main()
 {
   addressbook abook;
   string name;
+  string address;
 
   while(1){
-    cout << "住所録に登録する名前を入力してください（終了するにはquitと入力してください）: ";
+    cout << "住所録に登録する名前を入力してください（終了するにはquitと入力してください : ";
     cin >> name;
-    //   cout << "name.length() = " << name.length() << "\n";
-    if(name == "quit"){ break; }
-    abook.addFriend(name);
+    if(name=="quit"){ break; }
+    cout << "住所録に登録する住所を入力してください : ";
+    cin >> address;
+    abook.addFriend(name,address);
   }
 
   cout << "\n名前リスト:\n";
