ee982a8c037de51c3a3223d709a187c12eea9757
shumaikunkun <s1711520@gmail.com>
2019-12-21 18:34:38 +0900

    add k6.cpp

---
+++ k6.cpp
@@ -0,0 +1,29 @@
+/* sample 2 */
+#include <iostream>
+using namespace std;
+
+class person{
+protected:
+  string name;
+public:
+  void setName(string n){name = n;}  //セッター
+  string getName(){return name;}  //ゲッター
+};
+
+int main()
+{
+  int n=3; //人数
+  person friends[n];
+  string inputName;
+
+  for(int i=0; i<n; i++){
+    cout << i + 1 << "番目の名前は？";
+    cin >> inputName;
+    friends[i].setName(inputName);
+  }
+
+  cout << "\n名前リスト:\n";
+  for(int i=0; i<n; i++){
+    cout << " " << friends[i].getName() << "\n";
+  }
+}
