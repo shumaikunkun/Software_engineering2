03b7ba3b2be878f004b7030394d3430d904c8936
shumaikunkun <s1711520@gmail.com>
2019-12-26 19:52:32 +0900

    add h10

---
+++ h10.cpp
@@ -0,0 +1,53 @@
+//汎用関数
+#include <iostream>
+#include <random>
+#define N 4
+#define RMAX 10
+using namespace std;
+// defines a generic class
+template <class X> class myArray{
+public:
+  X data[N];
+  X max();
+};
+// defines a generic function
+template <class X> X myArray<X>::max(){
+  X largest;
+  for(int i = 1; i < sizeof(data)/sizeof(data[0]); i++){ if(largest < data[i]){ largest = data[i]; }}
+  return largest;
+}
+int main(){
+  myArray<int> forInt={1,2,5,3};
+  myArray<double> forDouble={0.1,0.2,0.5,0.2};
+  myArray<char> forChar={'a','b','x','c'};
+  myArray<bool> forBool={true,false,true,false};
+
+  cout << "forInt.max() = " << forInt.max() << endl;
+  cout << "forDouble.max() = " << forDouble.max() << endl;
+  cout << "forChar.max() = " << forChar.max() << endl;
+  cout << "forBool.max() = " << (forBool.max()?"true":"false") << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+// template <class X> X max(X &a){
+//
+// }
+//
+// int main(){
+//   int arr1[10]={1,2,3,4,5};
+//   char arr2[10]="abcde";
+//   bool arr3[10]={true,false,true,false,true};
+//
+//   cout << (2<1 ? "true":"false");
+//   cout << ('a'<'b' ? "true":"false");
+//   cout << (false<true ? "true":"false");
+// }
